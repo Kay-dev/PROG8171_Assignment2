6c5061c3c6b24a0ce61c85cdcb29141d39495fc1
zhengwei <a932327192@gmail.com>
2024-06-07 16:17:19 -0400

    add class:Product.cs

---
+++ PROG8171_Assignment2/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8171_Assignment2
+{
+    internal class Product
+    {
+        public Product(int id, string name, double price, int stock)
+        {
+            ProductID = id;
+            ProductName = name;
+            Price = price;
+            Stock = stock;
+        }
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+
+        public void IncreaseStock(int count)
+        {
+            Stock += count;
+        }
+
+        public void DecreaseStock(int count) 
+        {
+            Stock -= count; 
+        }
+
+    }
+}
